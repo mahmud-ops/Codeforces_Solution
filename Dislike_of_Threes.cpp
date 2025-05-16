944d4da7b8da0c00cd3dc5edabf8a6064c86bb64
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2025-05-16 20:50:47 +0600

    3

--- Dislike_of_Threes.cpp
+++ Dislike_of_Threes.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 int main(){
 
-    
+    int t , n;
+    cin >> t;
+
+    vector<int> num;
+
+    for(int i = 0 ; i < 1000 ; i++){
+        if(i%3 == 0 || i%10 == 3){
+            continue;
+        }
+        else{
+            num.push_back(i);
+        }
+    }
+
+    for(int i = 0 ; i < t ; i++){
+        cin >> n;
+        cout << num[n-1] << endl;
+    }
 
     return 0;
 }
